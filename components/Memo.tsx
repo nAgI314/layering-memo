d6c2270bc69d2328cc9bfaebdb82ed174ef286b3
nAgI314 <kurosawasyunnagi@gmail.com>
2025-06-20 16:00:10 +0900

    とりあえず表に

--- components/Memo.tsx
+++ components/Memo.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Image, StyleSheet } from 'react-native';
 
 interface MemoLayer { 
   contents : (MemoLayer | string )[],
@@ -15,18 +16,53 @@ export const Memo = () => {
     }
   );
 
+
+
   return(
-  <>
-  
-  </>
+  <div>
+  <MemoLayer/>
+  </div>
   )
 }
 
 const MemoLayer = () => {
+  const [memoContent,setMemoContent] = useState<string>("memo");
+  const write = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const writed = e.target.value;
+    setMemoContent(writed);
+  } 
+  const makeLayer = () => {
 
+  }
   return(
-  <>
-  
-  </>
+  <div>
+    <table border={1}>
+      <tr>
+        <th style={styles.th}>内容</th>
+        <th style={styles.th}>掘る</th>  
+      </tr>
+      <tr>
+        <td><textarea onChange={write}>{memoContent}</textarea></td>
+        <td><Image style={styles.Image} source={require("../assets/images/shovel-white-small.png")} /></td>
+      </tr>
+    
+    </table>
+  </div>
   )
 }
+
+const styles = StyleSheet.create({
+  th:{
+    color:"#ffffff",
+  },
+  Image: {
+    width: 40,
+    height: 40
+  },
+  content: {
+    flex: 1,
+    padding: 32,
+    gap: 16,
+    overflow: 'hidden',
+  },
+});
